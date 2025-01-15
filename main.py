5fbb4a2b2fdb200ae4993bdd190670f7d48241fc
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2025-01-15 08:55:54 +0100

    screen setup, add bg image.

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 import pygame
 
 pygame.init()
-print("Cloud climber under construction ...")
-print("Press 'q' to quit")
+
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Cloud Climber")
+
+# Assets
+# TODO: Change background to a more quality image
+bg_image = pygame.image.load("assets/background.png").convert_alpha()
+bg_image = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Game loop
+run = True
+while run:
+    # draw background 
+    screen.blit(bg_image, (0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    # Update game screen
+    pygame.display.update()
+
+
+pygame.quit()
+
